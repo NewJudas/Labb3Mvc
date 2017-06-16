92e1865922354b55bfe44062ed98ce04ac7f8ef1
NewJudas <kenan.alic@telia.com>
2017-06-16 12:53:17 +0200

    FirstModels

---
+++ CarModel/CarModel/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarModel.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string RegNr { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public int CarTypeId { get; set; }
+        public virtual CarType CarType { get; set; }
+    }
+}
---
+++ CarModel/CarModel/Models/CarType.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CarModel.Models
+{
+    public class CarType
+    {
+        public int Id { get; set; }
+        public string Enginge { get; set; }
+        public List<Car> Cars;
+    }
+}
